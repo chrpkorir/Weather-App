9ef8ab75c018e1242cd73e1d09c346493e21e492
chrpkorir <faythkoko@gmail.com>
2021-10-05 23:47:18 +0300

    Added weather,js file

--- src/App.js
+++ src/App.js
@@ -3,40 +3,104 @@ import './App.css';
 import {useState, useEffect} from "react";
 import Weather from "./Components/Weather";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import tasks from "./Components/task";
-function App() {
-    const [lat, setLat] = useState([]);
-    const [long, setLong] =useState([]);
-    const [data, setData] = useState([]);
+import 'weather-icons/css/weather-icons.css';
 
-    useEffect(()=> {
-        const fetchData = async () => {
-        navigator.geolocation.getCurrentPosition(function (position){
-            setLat(position.coords.latitude);
-            setLong(position.coords.longitude);
+import {Component} from 'react';
 
-        });
-        await fetch(`$(process.env.REACT_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
-      });
+const API_key = "P9df0b2f8cc5f6fc2a9e9e68cd5b30e86";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            city: undefined,
+            country: undefined,
+            icon: undefined,
+            main: undefined,
+            celsius: undefined,
+            temp_min: undefined,
+            temp_max: undefined,
+            description: "",
+            error: false
+
         }
-        fetchData();
-    }, [lat,long])
+        this.getWeather();
 
-  return (
-    <div className="App">
-        <tasks/>
-        {/*{(typeof data.main !== 'undefined') ? (*/}
-        {/*    <Weather weatherData={data}/>*/}
-        {/*): (*/}
-        {/*    <div></div>*/}
-        {/*)}*/}
 
-    </div>
-  );
+        this.weatherIcon = {
+            Thunderstorm: "wi-thunderstorm",
+            Drizzle: "wi-sleet",
+            Rain: "wi-storm-show",
+
+
+        }
+    }
+    //function to convert kelvin to celsius
+        calCelsius (temp){
+            let cell = Math.floor(temp - 273.15)
+            return cell;
+        }
+
+        get_weatherIcon(icons,rangeid){
+        switch (true){
+            case rangeid >=200 && rangeid <= 232:
+                this.setState({icon:this.weatherIcon.Thunderstorm});
+                break;
+            case rangeid >=200 && rangeid <= 232:
+                this.setState({icon:this.weatherIcon.Thunderstorm});
+                break;
+            case rangeid >=200 && rangeid <= 232:
+                this.setState({icon:this.weatherIcon.Thunderstorm});
+                break;
+            case rangeid >=200 && rangeid <= 232:
+                this.setState({icon:this.weatherIcon.Thunderstorm});
+                break;
+            case rangeid >=200 && rangeid <= 232:
+                this.setState({icon:this.weatherIcon.Thunderstorm});
+                break;
+            case rangeid >=200 && rangeid <= 232:
+                this.setState({icon:this.weatherIcon.Thunderstorm});
+                break;
+            case rangeid >=200 && rangeid <= 232:
+                this.setState({icon:this.weatherIcon.Thunderstorm});
+                break;
+        }
+
+        }
+
+
+
+    getWeather = async () => {
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weater?q=London,uk&appid=${API_key}`)
+
+      const response = await api_call.json();
+
+      console.log(response);
+      this.setState({
+          city:response.name,
+          country:response.sys.country,
+          celsius: this.calCelsius(response.main.temp),
+          temp_min:this.calCelsius(response.main.temp_min),
+          temp_max:this.calCelsius(response.main.temp_max),
+          description:response.weather[0].description
+      })
+    }
+    render() {
+        return (
+            <div className="App">
+                <Weather
+                    city={this.state.city}
+                    country={this.state.country}
+                    temp_celsius={this.state.celsius}
+                    temp_min={this.state.temp_min}
+                    temp_max={this.state.temp_max}
+                    description={this.state.description}
+                    weatherIcon={this.state.icon}
+                />
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 
 import { Card } from "semantic-ui-react";
 
-const CardExampleCard =(weatherData) => (
-    <Card>
-        <Card.Content>
-            <Card.Header className="header">{weatherData.name} </Card.Header>
-            <Card.Header className="header">City Name: {weatherData.name}</Card.Header>
-            <p>Temprature: {weatherData.main.temp}</p>
-            <p>Sunrise: {weatherData.sys.sunrise}</p>
-            <p>Sunset: {weatherData.sys.sunset}</p>
-            <p>Description: {weatherData.weather[0].description}</p>
-        </Card.Content>
-    </Card>
+const Weather = (props) =>{
+    return(
+        <div className="container">
+        <div className="cards">
+            <h1>{props.city}, {props.country}</h1>
+            <h5 className="py-4">
+                <i className={`wi ${props.weatherIcon} display-1`}></i>
+            </h5>
+            <h1 className="py-0"> {props.temp_celsius}&deg;</h1>
 
-);
-export default CardExampleCard;
+             {/* show max and min temp  */}
+            {minmaxTemp(props.temp_min, props.temp_max)}
+            <h4 className="py-4"> {props.description}</h4>
+        </div>
+        </div>
+    )
+};
+
+function minmaxTemp(min,max){
+    return(
+        <h3><span className="py-4">{min}&deg;</span>
+            <span className="py-4">{max}&deg;</span>
+        </h3>
+    );
+
+}
+export default Weather;
